02ffa67a5faca77815a4300574fd1bd71c751ae9
Lautaro Sanchez <sanchezlautaro1103@gmail.com>
2024-04-24 23:18:42 -0300

    Ejercicios del 12 al 15

--- Ejercicio_10_14.js
+++ Ejercicio_10_14.js
@@ -1,12 +1,16 @@
 //Sumar solo los elementos pares de un array.
 
 var numeros= [32, 45, 73, 18, 29, 50, 67]
-
 var suma=0
 for (let i=0; i<numeros.length; i++){
     if(numeros[i] % 2 === 0){
-        
         suma=suma+numeros[i]
     }
 }
+
+const numeroPares = numeros.filter(numero => numero % 2 === 0)
+
+console.log("Los numeros pares son:" )
+console.log(numeroPares)
+console.log("La suma de los numeros pares es:" )
 console.log(suma)
---
+++ Ejercicio_15.js
@@ -0,0 +1,22 @@
+//15_Crear una funcion que reciba 3 parametros: minimo, maximo y divisor. Devolver una lista con los divisores del numero ingresado.
+
+function obtenerDivisibles(minimo, maximo, divisor) {
+    const listaDivisibles = [];
+    
+    
+    for (let i = minimo; i <= maximo; i++) {
+      if (i % divisor === 0) { 
+        listaDivisibles.push(i);
+    }
+  
+    return listaDivisibles; 
+  }
+  
+ 
+  const minimo = 1; 
+  const maximo = 20; 
+  const divisor = 3; 
+  
+  const resultado = obtenerDivisibles(minimo, maximo, divisor); 
+  console.log('Números divisibles por', divisor, 'entre', minimo, 'y', maximo, ':', resultado);
+}
